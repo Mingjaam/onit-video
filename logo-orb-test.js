2ab07c6e95031da4ff0ad38576292053db3a5b58
Mingjaam <kimminjae0627.kakao.com>
2026-06-01 09:02:31 +0900

    Fix MacBook reveal easing helper

--- logo-orb-test.js
+++ logo-orb-test.js
@@ -887,6 +887,10 @@ function easeInOutCubic(x) {
   return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
 }
 
+function easeOutCubic(x) {
+  return 1 - Math.pow(1 - x, 3);
+}
+
 function lerp(a, b, t) {
   return a + (b - a) * t;
 }
